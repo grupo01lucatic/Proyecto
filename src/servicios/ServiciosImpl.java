4f4192b8d885d0f586db35f38223cdf80eddb8ce
saghi <saguiarda@gmail.com>
2019-04-24 15:53:15 +0200

    1.1.11 crear IServicios con metodo vacio y la clase que lo implementa

---
+++ src/servicios/ServiciosImpl.java
@@ -0,0 +1,14 @@
+package servicios;
+
+public class ServiciosImpl implements IServicios {
+
+	public ServiciosImpl() {
+	}
+
+	// Este metodo se encagar de obtener los datos del fichero.
+	@Override
+	public void iniciarLectura() {
+
+	}
+
+}
