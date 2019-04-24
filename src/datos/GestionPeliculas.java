e6360afec8b47675cf1fc6453b6304567e754c33
Santi <santialovera20@gmail.com>
2019-04-24 15:36:05 +0200

    1.2.1 Creada interfaz de gestión de peliculas
    Creada interfaz IGestionPeliculas con el método "modificarPeliculas" y la clase que lo implementa.

---
+++ src/datos/GestionPeliculas.java
@@ -0,0 +1,11 @@
+package datos;
+
+public class GestionPeliculas implements IGestionPeliculas {
+	// Este método se encarga de modificar las películas.
+	@Override
+	public void modificarPeliculas() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
