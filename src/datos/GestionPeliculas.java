fe70ea204cf8195767214c6bcd3fdfa54e7125bd
Santi <santialovera20@gmail.com>
2019-04-25 08:40:33 +0200

    Cleanup

--- src/datos/GestionPeliculas.java
+++ src/datos/GestionPeliculas.java
@@ -1,6 +1,5 @@
 package datos;
 
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -10,67 +9,59 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import model.Pelicula;
 import servicios.ConectorDB;
 
 public class GestionPeliculas implements IGestionPeliculas {
-	
+
 	static ConectorDB conexion = new ConectorDB();
 	static Connection con = null;
 	PreparedStatement pst = null;
 	Statement stm = null;
 	ResultSet rs = null;
-	
 
-	// Este mÃ©todo se encarga de modificar las pelÃ­culas.
+	/* Este método se encarga de modificar las películas. */
 	@Override
 	public void modificarPeliculas() {
 		// TODO Auto-generated method stub
 
 	}
 
-	
 	public void insertarPeliculas() throws IOException {
 		con = conexion.conectar();
-			
-			 try
-			    {
-			        BufferedReader br = new BufferedReader(new FileReader("peliculas_numCat.txt"));
-			       
 
-			        int c= 1;
-			        String line = null;
-			        while ((line = br.readLine()) != null)
-			        {
-			            String tmp[] = line.split(","); 
-			            String titulo = tmp[0].trim();
-			            String anio = tmp[1].trim();
-			            String id_categoria = tmp[2].trim();
-			            int id_pelicula = c++;
-			            String query =
-			                    "INSERT INTO peliculas (id_pelicula,titulo,anio,id_categoria) values (?,?,?,?)";
-			            try {
-							pst = con.prepareStatement(query);
-							pst.setInt(1, id_pelicula);
-							pst.setString(2, titulo);
-							pst.setInt(3, Integer.parseInt(anio));
-							pst.setInt(4, Integer.parseInt(id_categoria));
-							pst.execute();
-						} catch (SQLException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-						
-			            System.out.println(id_pelicula + "\t" + titulo + "\t" + anio + "\t" + id_categoria );
-			           
-			        }
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("peliculas_numCat.txt"));
 
-			        br.close();
-			        
+			int c = 1;
+			String line = null;
+			while ((line = br.readLine()) != null) {
+				String tmp[] = line.split(",");
+				String titulo = tmp[0].trim();
+				String anio = tmp[1].trim();
+				String id_categoria = tmp[2].trim();
+				int id_pelicula = c++;
+				String query = "INSERT INTO peliculas (id_pelicula,titulo,anio,id_categoria) values (?,?,?,?)";
+				try {
+					pst = con.prepareStatement(query);
+					pst.setInt(1, id_pelicula);
+					pst.setString(2, titulo);
+					pst.setInt(3, Integer.parseInt(anio));
+					pst.setInt(4, Integer.parseInt(id_categoria));
+					pst.execute();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+				System.out.println(id_pelicula + "\t" + titulo + "\t" + anio + "\t" + id_categoria);
+
+			}
+
+			br.close();
 
 		} finally {
 			try {
-				
+
 				if (con != null) {
 					con.close();
 				}
@@ -78,9 +69,7 @@ public class GestionPeliculas implements IGestionPeliculas {
 				e2.printStackTrace();
 			}
 		}
-		
-		
-	}
 
+	}
 
 }
