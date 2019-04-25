7b1ae32da19932be8d0bfd62eec3f4169e822de2
Jara Dominguez <jaradominguezavila@gmail.com>
2019-04-25 14:51:31 +0200

    1.2.3.1.2
    Creacion de metodo que maneje exception de NumberFormat... en datos

--- src/control/Main.java
+++ src/control/Main.java
@@ -2,9 +2,11 @@ package control;
 
 import java.io.IOException;
 
+import excepciones.MovieflixException;
+
 public class Main {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws MovieflixException {
 
 	}
 
