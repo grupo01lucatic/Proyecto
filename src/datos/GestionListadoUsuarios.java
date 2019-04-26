37f59125144a859fb2428a34b4ed2539076b8b67
Jara Dominguez <jaradominguezavila@gmail.com>
2019-04-26 08:58:18 +0200

    3.1.2
    Creado algoritmo en metodo listarUsuarios

--- src/datos/GestionListadoUsuarios.java
+++ src/datos/GestionListadoUsuarios.java
@@ -1,10 +1,63 @@
 package datos;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import excepciones.MovieflixException;
+import servicios.ConectorDB;
+
 public class GestionListadoUsuarios implements IGestionListadoUsuarios {
-	/* Método para mostrar la lista de usuarios */
+
+	private static Logger logger = (Logger) LogManager.getLogger(MovieflixException.class);
+
+	static ConectorDB conexion = new ConectorDB();
+	static Connection con = null;
+	PreparedStatement pst = null;
+	Statement stm = null;
+	ResultSet rs = null;
+
 	@Override
+
+	/** Método para mostrar la lista de usuarios */
 	public void mostrarListaUsuarios() {
 		// TODO Auto-generated method stub
+		try {
+			String query = "select id_user, username, email from usuarios;";
+			con = conexion.conectar();
+			logger.info("Conexion creada");
+			pst = con.prepareStatement(query);
+			rs = pst.executeQuery();
+			while (rs.next()) {
+				System.out.println("");
+				System.out.print(rs.getInt(1));
+				System.out.print(": ");
+				System.out.print(rs.getString(2));
+				System.out.print(": ");
+				System.out.print(rs.getString(3));
+			}
+
+		} catch (SQLException e) {
+			logger.error("Ha ocurrido un error");
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (pst != null) {
+					pst.close();
+				}
+				if (con != null) {
+					con.close();
+				}
+			} catch (Exception e) {
+				logger.error("Algo a salido mal");
+			}
+		}
 
 	}
 
